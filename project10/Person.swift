1b54bda009a547479543571b61dbf9396461a998
karinahri <dolmatovakarina72@gmail.com>
2024-10-31 21:11:18 +0200

    first commit

---
+++ project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Karina Dolmatova on 31.10.2024.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Karina Dolmatova on 31.10.2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
